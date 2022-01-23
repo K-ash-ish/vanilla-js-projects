3ac25ae32defc77cacd69c26f7fd2e313265098c
K-ash-ish <57871796+K-ash-ish@users.noreply.github.com>
2022-01-23 14:41:40 +0530

    Update index.js

--- scroll-project/index.js
+++ scroll-project/index.js
@@ -1,1 +1,32 @@
-//project
+const date = document.getElementById("date").textContent = new Date().getFullYear();
+const navBar = document.getElementsByTagName('nav');
+const scrollTop = document.querySelector(".scroll-to-top")
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 80){
+        navBar[0].classList.add("nav-bar-scroll")
+        scrollTop.classList.add("show-scroll-to-top")
+    }
+    else{
+        navBar[0].classList.remove("nav-bar-scroll")
+        scrollTop.classList.remove("show-scroll-to-top")
+    }
+
+});
+
+const scrollLinks = document.querySelectorAll(".scroll-links");
+
+scrollLinks.forEach( function(links){
+    links.addEventListener('click', function(e){
+        e.preventDefault();
+        const id = e.target.getAttribute('href').slice(1);
+        const el = document.getElementById(id);
+        const navHeight = navBar[0].getBoundingClientRect().height;
+
+        let position = el.offsetTop-navHeight;
+        window.scrollTo({
+            left:0, top: position,
+        });
+    })
+})
+

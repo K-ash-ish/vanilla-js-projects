d770c9dfe1cf0314240c10a9176a1e6950c3dea4
K-ash-ish <57871796+K-ash-ish@users.noreply.github.com>
2022-03-05 14:23:54 +0530

    Update index.js

--- dark-mode/index.js
+++ dark-mode/index.js
@@ -1,2 +1,22 @@
 //to tooggle between dark and light mode
+const toggleBtn = document.querySelector(".toggle-btn");
+toggleBtn.addEventListener("click", ()=>{
+    changeIcon(toggleBtn)
+})
 
+function changeIcon(btn){
+    let rootEl = document.body;
+    rootEl.classList.toggle("dark-mode")
+    if(btn.classList.contains("fa-moon")){
+        // console.log("dark mode ON")
+        btn.classList.remove("fa-moon")
+        btn.classList.toggle("fa-sun")
+    }
+    else if(btn.classList.contains("fa-sun")){
+        btn.classList.toggle("fa-moon")
+        btn.classList.remove("fa-sun")
+        // console.log("light mode ON")
+        
+    }
+
+}
